471446c517d7d0d48e665eecffd32b0fa9fd7639
zeke <zeke@sikelianos.com>
2011-08-08 20:47:21 -0700

    It begins...

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Melitsa::Application.config.session_store :cookie_store, :key => '_melitsa_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Melitsa::Application.config.session_store :active_record_store
